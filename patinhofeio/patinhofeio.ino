6cb592565b626c49ee586e41f557fff9ee81e73a
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2017-03-18 11:48:12 -0300

    emulation of most of the remaining instructions

--- patinhofeio/patinhofeio.ino
+++ patinhofeio/patinhofeio.ino
@@ -30,6 +30,7 @@
 #define NUM_LEDS 80
 
 #define INDEX_REG 0
+#define EXTENSION_REG 1
 #define RAM_SIZE 256
 
 byte RAM[RAM_SIZE];
@@ -339,14 +340,82 @@ byte opcode;
 CPU Instructions implementation
 ********************************/
 
+void PLA_instruction(){
+  /* OPCODE: 0x0X */
+  //PLA = "Pula": Jump to address
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  addr = compute_effective_address(addr);
+  inc_CI();
+  CI(addr);
+}
+
+void PLAX_instruction(){
+  /* OPCODE: 0x1X */
+  //PLAX = "Pula indexado": Jump to indexed address
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  inc_CI();
+  byte idx = read_index_reg();
+  addr = compute_effective_address(idx + addr);
+  CI(addr);
+}
+
+void ARM_instruction(){
+  /* OPCODE: 0x2X */
+  //ARM = "Armazena": Store the value of the accumulator into a given memory position
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  addr = compute_effective_address(addr);
+  inc_CI();
+  write_ram(addr, _ACC);
+}
+
+void ARMX_instruction(){
+  /* OPCODE: 0x3X */
+  //ARMX = "Armazena indexado": Store the value of the accumulator into a given indexed memory position
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  inc_CI();
+  byte idx = read_index_reg();
+  addr = compute_effective_address(idx + addr);
+  write_ram(addr, _ACC);
+}
+
+void CAR_instruction(){
+  /* OPCODE: 0x4X */
+  //CAR = "Carrega": Load a value from a given memory position into the accumulator
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  inc_CI();
+  addr = compute_effective_address(addr);
+  ACC(read_ram(addr));
+}
+
 void CARX_instruction(){
-  /* OPCODE: 0x50 */
+  /* OPCODE: 0x5X */
   //CARX = "Carga indexada": Load a value from a given indexed memory position into the accumulator
-  int tmp = (opcode & 0x0F) << 8 | read_ram(_CI);
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
   inc_CI();
   int idx = read_index_reg();
-  //TODO: compute_effective_address(m_idx + tmp);
-  ACC(read_ram(idx + tmp));
+  addr = compute_effective_address(idx + addr);
+  ACC(read_ram(addr));
+}
+
+void SOM_instruction(){
+  /* OPCODE: 0x6X */
+  //SOM = "Soma": Add a value from a given memory position into the accumulator
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  addr = compute_effective_address(addr);
+  inc_CI();
+  ACC(_ACC + read_ram(addr));
+  //TODO: update V and T flags
+}
+
+void SOMX_instruction(){
+  /* OPCODE: 0x7X */
+  //SOMX = "Soma indexada": Add a value from a given indexed memory position into the accumulator
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  inc_CI();
+  byte idx = read_index_reg();
+  addr = compute_effective_address(idx + addr);
+  ACC(_ACC + read_ram(addr));
+  //TODO: update V and T flags
 }
 
 void LIMPO_instruction(){
@@ -358,6 +427,46 @@ void LIMPO_instruction(){
   VAI_UM(0);
 }
 
+void UM_instruction(){
+  /* OPCODE: 0x81 */
+  //UM="One":
+  //    Load 1 into accumulator
+  //    and clear the flags
+  ACC(1);
+  TRANSBORDO(0);
+  VAI_UM(0);
+}
+
+void CMP1_instruction(){
+  /* OPCODE: 0x82 */
+  //CMP1:
+  // Compute One's complement of the accumulator
+  //    and clear the flags
+  ACC(~_ACC);
+  TRANSBORDO(0);
+  VAI_UM(0);
+}
+
+void CMP2_instruction(){
+  /* OPCODE: 0x83 */
+  //CMP2:
+  // Compute Two's complement of the accumulator
+  //    and updates flags according to the result of the operation
+  ACC(~_ACC + 1);
+
+  //TODO: fix-me (I'm not sure yet how to compute the flags here):
+  TRANSBORDO(0);
+  VAI_UM(0);
+}
+
+void LIM_instruction(){
+  /* OPCODE: 0x84 */
+  //LIM="Limpa":
+  // Clear flags
+  TRANSBORDO(0);
+  VAI_UM(0);
+} 
+
 void INC_instruction(){
   /* OPCODE: 0x85 */
   //INC:
@@ -365,6 +474,206 @@ void INC_instruction(){
   ACC(_ACC+1);
   TRANSBORDO(0); //TODO: fix-me (I'm not sure yet how to compute the flags here)
   VAI_UM(0); //TODO: fix-me (I'm not sure yet how to compute the flags here)
+}
+
+void UNEG_instruction(){
+  /* OPCODE: 0x86 */
+  //UNEG="Um Negativo":
+  // Load -1 into accumulator and clear flags
+  ACC(-1);
+  TRANSBORDO(0);
+  VAI_UM(0);
+}
+
+void LIMP1_instruction(){
+  /* OPCODE: 0x87 */
+  //LIMP1:
+  //    Clear accumulator, reset T and set V
+  ACC(0);
+  TRANSBORDO(0);
+  VAI_UM(1);
+}
+
+void PNL_0_instruction(){
+  /* OPCODE: 0x88 */
+  //PNL 0:
+  ACC(_DADOS_DO_PAINEL & 0xFF);
+  TRANSBORDO(0);
+  VAI_UM(0);
+}
+
+void PNL_1_instruction(){
+  /* OPCODE: 0x89 */
+  //PNL 1:
+  ACC((_DADOS_DO_PAINEL & 0xFF) + 1);
+  //TODO: TRANSBORDO(?);
+  //TODO: VAI_UM(?);
+}
+
+void PNL_2_instruction(){
+  /* OPCODE: 0x8A */
+  //PNL 2:
+  ACC((_DADOS_DO_PAINEL & 0xFF) - _ACC - 1);
+  //TODO: TRANSBORDO(?);
+  //TODO: VAI_UM(?);
+}
+
+void PNL_3_instruction(){
+  /* OPCODE: 0x8B */
+  //PNL 3:
+  ACC((_DADOS_DO_PAINEL & 0xFF) - _ACC);
+  //TODO: TRANSBORDO(?);
+  //TODO: VAI_UM(?);
+}
+
+void PNL_4_instruction(){
+  /* OPCODE: 0x8C */
+  //PNL 4:
+  ACC((_DADOS_DO_PAINEL & 0xFF) + _ACC);
+  //TODO: TRANSBORDO(?);
+  //TODO: VAI_UM(?);
+}
+
+void PNL_5_instruction(){
+  /* OPCODE: 0x8D */
+  //PNL 5:
+  ACC((_DADOS_DO_PAINEL & 0xFF) + _ACC + 1);
+  //TODO: TRANSBORDO(?);
+  //TODO: VAI_UM(?);
+}
+
+void PNL_6_instruction(){
+  /* OPCODE: 0x8E */
+  //PNL 6:
+  ACC((_DADOS_DO_PAINEL & 0xFF) - 1);
+  //TODO: TRANSBORDO(?);
+  //TODO: VAI_UM(?);
+}
+
+void PNL_7_instruction(){
+  /* OPCODE: 0x8F */
+  //PNL 7:
+  ACC(_DADOS_DO_PAINEL & 0xFF);
+  VAI_UM(1);
+}
+
+void ST_0_instruction(){
+  /* OPCODE: 0x90 */
+  //ST 0 = "Se T=0, Pula"
+  //       If T is zero, skip the next instruction
+  if (_TRANSBORDO == 0){
+    inc_CI(); //skip
+  }
+}
+
+void STM_0_instruction(){
+  /* OPCODE: 0x91 */
+  //STM 0 = "Se T=0, Pula e muda"
+  //        If T is zero, skip the next instruction
+  //        and toggle T.
+  if (_TRANSBORDO == 0){
+    inc_CI(); //skip
+    TRANSBORDO(1);
+  }
+}
+
+void ST_1_instruction(){
+  /* OPCODE: 0x92 */
+  //ST 1 = "Se T=1, Pula"
+  //       If T is one, skip the next instruction
+  if (_TRANSBORDO == 1){
+    inc_CI(); //skip
+  }
+}
+
+void STM_1_instruction(){
+  /* OPCODE: 0x93 */
+  //STM 1 = "Se T=1, Pula e muda"
+  //        If T is one, skip the next instruction
+  //        and toggle T.
+  if (_TRANSBORDO == 1){
+    inc_CI(); //skip
+    TRANSBORDO(0);
+  }
+}
+
+void SV_0_instruction(){
+  /* OPCODE: 0x94 */
+  //SV 0 = "Se V=0, Pula"
+  //       If V is zero, skip the next instruction
+  if (_VAI_UM == 0){
+    inc_CI(); //skip
+  }
+}
+
+void SVM_0_instruction(){
+  /* OPCODE: 0x95 */
+  //SVM 0 = "Se V=0, Pula e muda"
+  //        If V is zero, skip the next instruction
+  //        and toggle V.
+  if (_VAI_UM == 0){
+    inc_CI(); //skip 
+    VAI_UM(1);
+  } 
+} 
+
+void SV_1_instruction(){
+  /* OPCODE: 0x96 */
+  //SV 1 = "Se V=1, Pula"
+  //       If V is one, skip the next instruction
+  if (_VAI_UM == 1){
+    inc_CI(); //skip 
+  } 
+} 
+
+void SVM_1_instruction(){
+  /* OPCODE: 0x97 */
+  //SVM 1 = "Se V=1, Pula e muda"
+  //        If V is one, skip the next instruction
+  //        and toggle V.
+  if (_VAI_UM == 1){
+    inc_CI(); //skip 
+    VAI_UM(0);
+  } 
+}
+
+void PUL_instruction(){
+  /* OPCODE: 0x98 */
+  //PUL="Pula para /002 a limpa estado de interrupcao"
+  //     Jump to address /002 and disables interrupts
+  CI(0x002);
+  EXTERNO(0); //TODO: verify this!
+}
+
+void TRE_instruction(){
+  /* OPCODE: 0x99 */
+  //TRE="Troca conteudos de ACC e EXT"
+  //     Exchange the value of the accumulator with the ACC extension register
+  byte value = _ACC;
+  ACC(read_ram(EXTENSION_REG));
+  write_ram(EXTENSION_REG, value);
+}
+
+void INIB_instruction(){
+  /* OPCODE: 0x9A */
+  //INIB="Inibe"
+  //     disables interrupts
+  //TODO: Implement-me!  m_interrupts_enabled = false;
+}
+
+void PERM_instruction(){
+  /* OPCODE: 0x9B */
+  //PERM="Permite"
+  //     enables interrupts
+  //TODO: Implement-me!  m_interrupts_enabled = true;
+}
+
+void ESP_instruction(){
+ /* OPCODE: 0x9C */
+ //ESP="Espera":
+ //    Holds execution and waits for an interrupt to occur.
+ //TODO:  m_run = false;
+ //TODO:  m_wait_for_interrupt = true;
 }
 
 void PARE_instruction(){
@@ -386,60 +695,6 @@ void TRI_instruction(){
   write_index_reg(value);
 }
 
-void XOR_instruction(){
-  /* OPCODE: 0xD2 */
-  //XOR: Computes the bitwise XOR of an immediate into the accumulator
-  ACC(_ACC ^ read_ram(_CI));
-  inc_CI();
-  //TODO: update T and V flags
-}
-
-void NAND_instruction(){
-  /* OPCODE: 0xD4 */
-  //NAND: Computes the bitwise XOR of an immediate into the accumulator
-  ACC(~(_ACC & read_ram(_CI)));
-  inc_CI();
-  //TODO: update T and V flags
-}
-
-void SOMI_instruction(){
-  /* OPCODE: 0xD8 */
-  //SOMI="Soma Imediato":
-  //Add an immediate into the accumulator
-  //TODO: set_flag(V, ((((int16_t) ACC) + ((int16_t) READ_BYTE_PATINHO(PC))) >> 8));
-  //TODO: set_flag(T, ((((int8_t) (ACC & 0x7F)) + ((int8_t) (READ_BYTE_PATINHO(PC) & 0x7F))) >> 7) == V);
-  ACC(_ACC + read_ram(_CI));
-  inc_CI();
-}
-
-void PLA_instruction(){
-  /* OPCODE: 0x0X */
-  //PLA = "Pula": Jump to address
-  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
-  addr = compute_effective_address(addr);
-  inc_CI();
-  CI(addr);
-}
-
-void ARM_instruction(){
-  /* OPCODE: 0x2X */
-  //ARM = "Armazena": Store the value of the accumulator into a given memory position
-  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
-  addr = compute_effective_address(addr);
-  inc_CI();
-  write_ram(addr, _ACC);
-}
-
-void SOM_instruction(){
-  /* OPCODE: 0x6X */
-  //SOM = "Soma": Add a value from a given memory position into the accumulator
-  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
-  addr = compute_effective_address(addr);
-  inc_CI();
-  ACC(_ACC + read_ram(addr));
-  //TODO: update V and T flags
-}
-
 void PLAN_instruction(){
   /* OPCODE: 0xAX */
   //PLAN = "Pula se ACC negativo": Jump to a given address if ACC is negative
@@ -451,12 +706,20 @@ void PLAN_instruction(){
   }
 }
 
-void SAI_instruction(byte channel){
-  /* OPCODE: 0xCX 0x8X */
-  /* SAI = "Output data to I/O device" */
-  //TODO: handle multiple device channels: m_iodev_write_cb[channel](ACC);
-  delay(1000/300); //This is REALLY BAD emulation-wise but it looks nice :-)
-  Serial.write(_ACC);
+void PLAZ_instruction(){
+  /* OPCODE: 0xBX */
+  //PLAZ = "Pula se ACC for zero": Jump to a given address if ACC is zero
+  int addr = (opcode & 0x0F) << 8 | read_ram(_CI);
+  addr = compute_effective_address(addr);
+  inc_CI();
+  if (_ACC == 0){
+    CI(addr);
+  }
+}
+
+void FNC_instruction(byte channel, byte function){
+  /* OPCODE: 0xCX 0x1X */
+  //Implement-me!
 }
 
 void SAL_instruction(byte channel, byte function){
@@ -492,7 +755,88 @@ void SAL_instruction(byte channel, byte function){
   }
 }
 
+void ENTR_instruction(byte channel){
+  /* OPCODE: 0xCX 0x4X */
+  /* ENTR = "Input data from I/O device" */
+  //TODO: handle multiple device channels: m_iodev_write_cb[channel](ACC);
+  //Implement-me!
+}
 
+void SAI_instruction(byte channel){
+  /* OPCODE: 0xCX 0x8X */
+  /* SAI = "Output data to I/O device" */
+  //TODO: handle multiple device channels: m_iodev_write_cb[channel](ACC);
+  delay(1000/300); //This is REALLY BAD emulation-wise but it looks nice :-)
+  Serial.write(_ACC);
+}
+
+void IO_instructions(){
+  //Executes I/O functions
+  //TODO: Implement-me!
+  byte value = read_ram(_CI);
+  inc_CI();
+  byte channel = opcode & 0x0F;
+  byte function = value & 0x0F;
+  switch(value & 0xF0){
+    case 0x10: FNC_instruction(channel, function); return;
+    case 0x20: SAL_instruction(channel, function); return;
+    case 0x40: ENTR_instruction(channel); return;
+    case 0x80: SAI_instruction(channel); return;
+  }
+}
+
+void SUS_instruction(){
+  /* OPCODE: 0xEX */
+  //TODO: Implement-me!
+}
+
+void PUG_instruction(){
+  /* OPCODE: 0xFX */
+  //TODO: Implement-me!
+}
+
+
+void XOR_instruction(){
+  /* OPCODE: 0xD2 */
+  //XOR: Computes the bitwise XOR of an immediate into the accumulator
+  ACC(_ACC ^ read_ram(_CI));
+  inc_CI();
+  //TODO: update T and V flags
+}
+
+void NAND_instruction(){
+  /* OPCODE: 0xD4 */
+  //NAND: Computes the bitwise XOR of an immediate into the accumulator
+  ACC(~(_ACC & read_ram(_CI)));
+  inc_CI();
+  //TODO: update T and V flags
+}
+
+void SOMI_instruction(){
+  /* OPCODE: 0xD8 */
+  //SOMI="Soma Imediato":
+  //Add an immediate into the accumulator
+  //TODO: set_flag(V, ((((int16_t) ACC) + ((int16_t) READ_BYTE_PATINHO(PC))) >> 8));
+  //TODO: set_flag(T, ((((int8_t) (ACC & 0x7F)) + ((int8_t) (READ_BYTE_PATINHO(PC) & 0x7F))) >> 7) == V);
+  ACC(_ACC + read_ram(_CI));
+  inc_CI();
+}
+
+void CARI_instruction(){
+  /* OPCODE: 0xDA */
+  //CARI="Carrega Imediato":
+  //     Load an immediate into the accumulator
+  ACC(read_ram(_CI));
+  inc_CI();
+}
+
+
+void IND_instruction(){
+  //TODO: Implement-me!
+}
+void shift_rotate_instructions(){
+  //TODO: Implement-me!
+}
 
 void run_one_instruction(){
   unsigned int tmp;
@@ -511,33 +855,63 @@ void run_one_instruction(){
   inc_CI();
 
   switch (opcode){
-    case 0x50: CARX_instruction(); return;
     case 0x80: LIMPO_instruction(); return;
+    case 0x81: UM_instruction(); return;
+    case 0x82: CMP1_instruction(); return;
+    case 0x83: CMP2_instruction(); return;
+    case 0x84: LIM_instruction(); return;
     case 0x85: INC_instruction(); return;
+    case 0x86: UNEG_instruction(); return;
+    case 0x87: LIMP1_instruction(); return;
+    case 0x88: PNL_0_instruction(); return;
+    case 0x89: PNL_1_instruction(); return;
+    case 0x8A: PNL_2_instruction(); return;
+    case 0x8B: PNL_3_instruction(); return;
+    case 0x8C: PNL_4_instruction(); return;
+    case 0x8D: PNL_5_instruction(); return;
+    case 0x8E: PNL_6_instruction(); return;
+    case 0x8F: PNL_7_instruction(); return;
+    case 0x90: ST_0_instruction(); return;
+    case 0x91: STM_0_instruction(); return;
+    case 0x92: ST_1_instruction(); return;
+    case 0x93: STM_1_instruction(); return;
+    case 0x94: SV_0_instruction(); return;
+    case 0x95: SVM_0_instruction(); return;
+    case 0x96: SV_1_instruction(); return;
+    case 0x97: SVM_1_instruction(); return;
+    case 0x98: PUL_instruction(); return;
+    case 0x99: TRE_instruction(); return;
+    case 0x9A: INIB_instruction(); return;
+    case 0x9B: PERM_instruction(); return;
+    case 0x9C: ESP_instruction(); return;
     case 0x9D: PARE_instruction(); return;
     case 0x9E: TRI_instruction(); return;
+    case 0x9F: IND_instruction(); return;
+    case 0xD1: shift_rotate_instructions(); return;
     case 0xD2: XOR_instruction(); return;
     case 0xD4: NAND_instruction(); return;
     case 0xD8: SOMI_instruction(); return;
+    case 0xDA: CARI_instruction(); return;
   }
 
   switch (opcode & 0xF0){
     case 0x00: PLA_instruction(); return;
+    case 0x10: PLAX_instruction(); return;
     case 0x20: ARM_instruction(); return;
+    case 0x30: ARMX_instruction(); return;
+    case 0x40: CAR_instruction(); return;
+    case 0x50: CARX_instruction(); return;
     case 0x60: SOM_instruction(); return;
+    case 0x70: SOMX_instruction(); return;
+    //case 0x80: single byte instructions
+    //case 0x90:     declared above
     case 0xA0: PLAN_instruction(); return;
-    case 0xC0:
-      //Executes I/O functions
-      //TODO: Implement-me!
-      value = read_ram(_CI);
-      inc_CI();
-      channel = opcode & 0x0F;
-      function = value & 0x0F;
-      switch(value & 0xF0){
-        //TODO: case 0x10: ...
-	    case 0x80: SAI_instruction(channel); return;
-        case 0x20: SAL_instruction(channel, function); return;
-      }
+    case 0xB0: PLAZ_instruction(); return;
+    case 0xC0: IO_instructions(); return;
+    //case 0xD0: shift/rotate, XOR, NAND,
+    //           SOMI, CARI instructions above
+    case 0xE0: SUS_instruction(); return;
+    case 0xF0: PUG_instruction(); return;
     default:
       Serial.print("OPCODE INVALIDO: ");
       Serial.println(opcode, HEX);
